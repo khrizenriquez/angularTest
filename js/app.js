36c5dff0c9c868485e66a008f2b3fbb6ff654995
Khriz Enríquez <christoferen7@gmail.com>
2015-09-29 12:16:52 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+*Author     @khrizenriquez
+*Course:    https://www.udemy.com/quickstart-angularjs/learn/#/
+*/
+
+var HeaderCtrl = function ($scope) {
+	$scope.details = {
+		title: 'BooKart',
+		description: 'Tenemos un millon de libros'
+	};
+}
